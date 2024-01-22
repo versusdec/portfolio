b8dde5618da083dae060168d9a78690f8261b310
versusdec <versusdec@gmail.com>
2024-01-22 11:31:48 +0200

    update

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,10 +1,27 @@
-import Styles from './styles.module.scss'
 import { Button } from "../../components/button";
 import { GitHub as GitHubIcon, GitHub } from "@mui/icons-material";
 import { SvgIcon } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Modal } from "../../components/modal";
+import { Recorder } from "../../components/Recorder"
+import Styles from './styles.module.scss'
 
 const Portfolio = () => {
+  const [playerInit, setPlayerInit] = useState(false)
+  const [open, setOpen] = useState(false)
+  
+  useEffect(() => {
+    if (!playerInit) {
+      Recorder(document.getElementById('player'), {record: true, file: '/recorder/example.mp3'})
+      setPlayerInit(true)
+    }
+    /*Dropzone(app, {
+      accept: '.mp3',
+      onChange: (files) => {
+        recorder.updateFile(files[0])
+      }
+    })*/
+  }, [playerInit])
   
   return (
     <main className={Styles.main}>
@@ -12,7 +29,7 @@ const Portfolio = () => {
       <div className={Styles.list}>
         <div className={Styles.item}>
           <picture>
-            <img src="/assets/img/botto.png" alt="botto"/>
+            <img src="/assets/img/botto.png" alt="botto" loading={'lazy'}/>
           </picture>
           <aside>
             <h3>Communication Platform</h3>
@@ -28,7 +45,7 @@ const Portfolio = () => {
             </ul>
             <div className={Styles.preview}>
               <a href={'/botto'} target={'_blank'}>
-                <Button>preview</Button>
+                <Button secondary>preview</Button>
               </a>
               <a href="https://github.com/versusdec/versusdec.github.io/tree/gh-pages/botto" target={"_blank"} className={Styles.git}>
                 <SvgIcon><GitHub/></SvgIcon>
@@ -40,7 +57,7 @@ const Portfolio = () => {
       <div className={Styles.list}>
         <div className={Styles.item}>
           <picture>
-            <img src="/assets/img/yugo.png" alt="yugo"/>
+            <img src="/assets/img/yugo.png" alt="yugo" loading={'lazy'}/>
           </picture>
           <aside>
             <h3>Advertising Platform</h3>
@@ -56,7 +73,7 @@ const Portfolio = () => {
             </ul>
             <div className={Styles.preview}>
               <a href={'/yugo'} target={'_blank'}>
-                <Button>preview</Button>
+                <Button secondary>preview</Button>
               </a>
               <a href="https://github.com/versusdec/versusdec.github.io/tree/gh-pages/yugo" target={"_blank"} className={Styles.git}>
                 <SvgIcon><GitHub/></SvgIcon>
@@ -68,7 +85,7 @@ const Portfolio = () => {
       <div className={Styles.list}>
         <div className={Styles.item}>
           <picture>
-            <img src="/assets/img/estate.png" alt="estate"/>
+            <img src="/assets/img/estate.png" alt="estate" loading={'lazy'}/>
           </picture>
           <aside>
             <h3>Housing Estates</h3>
@@ -84,7 +101,7 @@ const Portfolio = () => {
             </ul>
             <div className={Styles.preview}>
               <a href={'/estate'} target={'_blank'}>
-                <Button>preview</Button>
+                <Button secondary>preview</Button>
               </a>
               <a href="https://github.com/versusdec/versusdec.github.io/tree/gh-pages/estate" target={"_blank"} className={Styles.git}>
                 <SvgIcon><GitHub/></SvgIcon>
@@ -94,6 +111,119 @@ const Portfolio = () => {
         </div>
       </div>
       <div className={Styles.list}>
+        <div className={Styles.item}>
+          <picture>
+            <img src="/assets/img/infrared.png" alt="infrared" loading={'lazy'}/>
+          </picture>
+          <aside>
+            <h3>Lawyers Company</h3>
+            <ul>
+              <li>
+                <span>Project : </span>
+                <span>Personal Website</span>
+              </li>
+              <li>
+                <span>Languages : </span>
+                <span>html, css, js, jq</span>
+              </li>
+            </ul>
+            <div className={Styles.preview}>
+              <a href={'/infrared'} target={'_blank'}>
+                <Button secondary>preview</Button>
+              </a>
+              <a href="https://github.com/versusdec/versusdec.github.io/tree/gh-pages/infrared" target={"_blank"} className={Styles.git}>
+                <SvgIcon><GitHub/></SvgIcon>
+              </a>
+            </div>
+          </aside>
+        </div>
+      </div>
+      <div className={Styles.list}>
+        <div className={Styles.item}>
+          <picture>
+            <img src="/assets/img/tdj.png" alt="tdj" loading={'lazy'}/>
+          </picture>
+          <aside>
+            <h3>Scooter Sharing</h3>
+            <ul>
+              <li>
+                <span>Project : </span>
+                <span>Landing Page</span>
+              </li>
+              <li>
+                <span>Languages : </span>
+                <span>html, css, js, jq</span>
+              </li>
+            </ul>
+            <div className={Styles.preview}>
+              <a href={'/tdj'} target={'_blank'}>
+                <Button secondary>preview</Button>
+              </a>
+              <a href="https://github.com/versusdec/versusdec.github.io/tree/gh-pages/tdj" target={"_blank"} className={Styles.git}>
+                <SvgIcon><GitHub/></SvgIcon>
+              </a>
+            </div>
+          </aside>
+        </div>
+      </div>
+      <div className={Styles.list}>
+        <div className={Styles.item}>
+          <picture>
+            <img src="/assets/img/vodoley.png" alt="vodoley" loading={'lazy'}/>
+          </picture>
+          <aside>
+            <h3>Sprinkle Machines</h3>
+            <ul>
+              <li>
+                <span>Project : </span>
+                <span>Landing Page</span>
+              </li>
+              <li>
+                <span>Languages : </span>
+                <span>html, css, js, jq</span>
+              </li>
+            </ul>
+            <div className={Styles.preview}>
+              <a href={'/vodoley'} target={'_blank'}>
+                <Button secondary>preview</Button>
+              </a>
+              <a href="https://github.com/versusdec/versusdec.github.io/tree/gh-pages/vodoley" target={"_blank"} className={Styles.git}>
+                <SvgIcon><GitHub/></SvgIcon>
+              </a>
+            </div>
+          </aside>
+        </div>
+      </div>
+      <div className={Styles.list}>
+        <div className={Styles.item}>
+          <picture>
+            <img src="/assets/img/vtb.png" alt="vtb" loading={'lazy'}/>
+          </picture>
+          <aside>
+            <h3>Digital Distribution</h3>
+            <ul>
+              <li>
+                <span>Project : </span>
+                <span>Landing Page</span>
+              </li>
+              <li>
+                <span>Languages : </span>
+                <span>html, css, js, jq</span>
+              </li>
+            </ul>
+            <div className={Styles.preview}>
+              <a href={'/vtb'} target={'_blank'}>
+                <Button secondary>preview</Button>
+              </a>
+              <a href="https://github.com/versusdec/versusdec.github.io/tree/gh-pages/vtb" target={"_blank"} className={Styles.git}>
+                <SvgIcon><GitHub/></SvgIcon>
+              </a>
+            </div>
+          </aside>
+        </div>
+      </div>
+      
+      <div className={Styles.list} style={{display: "none"}}>
         <div className={Styles.item}>
           <picture>
             <img src="/assets/img/keszflow.png" alt="keszflow"/>
@@ -112,7 +242,7 @@ const Portfolio = () => {
             </ul>
             <div className={Styles.preview}>
               <a href={'/keszflow/login'} target={'_blank'}>
-                <Button>preview</Button>
+                <Button secondary>preview</Button>
               </a>
               <a href="https://github.com/versusdec/keszflow" target={"_blank"} className={Styles.git}>
                 <SvgIcon><GitHub/></SvgIcon>
@@ -121,6 +251,15 @@ const Portfolio = () => {
           </aside>
         </div>
       </div>
+      <div id={'player'} className={Styles['player-wrapper']}>
+        <a title="github" target="_blank" rel="noopener" href="https://github.com/versusdec/versusdec.github.io/tree/gh-pages/recorder"><SvgIcon><GitHubIcon/></SvgIcon></a>
+        {/*<a className={Styles.upload} onClick={()=>{
+          setOpen(true)
+        }}>upload</a>*/}
+      </div>
+      {/*<Modal open={open}>
+        <h1>hello world</h1>
+      </Modal>*/}
     </main>
   )
 }
